cff23d48b3584b332b28094bbe11bdf7ae5b694e
Jorge Riboldi <jfriboldi@gmail.com>
2021-03-26 07:25:33 -0700

    starting styling

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-const Page = () => {
-  const router = useRouter()
-  const { id } = router.query
+export default () => {
+  const router = useRouter();
+  const { id } = router.query;
 
   return (
-    <h1>
-      Note {id}
-    </h1>
-  )
-}
-export default Page
+    <div>
+      <h1>Note: {id} </h1>
+
+      <Link href="/notes">
+        <a>Notes</a>
+      </Link>
+    </div>
+  );
+};
